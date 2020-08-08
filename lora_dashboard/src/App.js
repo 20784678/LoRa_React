469e5dc48bba169192143270caa05a5a231a7bef
Daniel Banks <20784678@sun.ac.za>
2020-08-08 07:30:34 +0200

    SideNav 01 + Button 01
    Added SideNav in and out functionality as well as pretty bootstrap button

--- lora_dashboard/src/App.js
+++ lora_dashboard/src/App.js
@@ -1,23 +1,48 @@
 import React from 'react';
 import PropTypes from "prop-types";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button'
+import SideNav from './Layouts/SideNav.js'
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 
 class App extends React.Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      sideNavOpen: true,
+      buttonPressed: false,
+      sideNavOpen: false,
     };
   }
 
 	render()	{
     const { classes, ...rest } = this.props;
+
     return (
-      <div style={{position: 'absolute', width: '100%', height: '100%', background: 'red'}}>
-        <div style={{width: '100px' ,height: '100%', background: 'blue' }}></div>
-          
-      </div>
+      
+        <div style={{position: 'absolute', width: '100%', height: '100%', background: 'white'}}> {/* background */}
+          <div style={{poisition: 'absolute', width:'100%',height:'90px',background:'lightblue',display:'flex', alignItems: 'center'}}> {/* top naivigation bar */}
+            <Button variant= {this.state.buttonPressed ? "primary":"outline-primary"} size="lg" onClick={() => this.buttonPressed(), this.sideNavToggle} style={{marginLeft: '10px'}}>
+              Primary
+              </Button>{' '}
+          </div>
+          <div style={{position: 'fixed',width: '100%', height:'calc(100%-90px)',left: this.state.sideNavOpen ? '0px' : '-300px',transition: 'all 0.3s ease'}}> {/*content container*/}
+            {/*Stick content here*/}
+            <SideNav></SideNav>
+          </div>
+        </div>
+      
     )
+  }
+
+  buttonPressed = () =>{ {/* Function that runs when onClick happens*/}
+    console.log('hello');
+    this.setState({buttonPressed: !this.state.buttonPressed});
+  }
+
+  sideNavToggle = () =>{
+    console.log('yoyoy bitch');
+    this.setState({sideNavOpen: !this.state.sideNavOpen});
   }
 
 }
---
+++ lora_dashboard/src/Layouts/SideNav.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button'
+import PropTypes from "prop-types";
+import { withRouter } from 'react-router-dom';
+
+
+class SideNav extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+	render()	{
+    const { classes, ...rest } = this.props;
+    return (
+      <div style={{position: 'fixed', width: '120px', height: '100%', background: 'lightgrey', display: 'flex', flexDirection:'column', alignItems: 'center'}}>
+          <Button variant="outline-primary" style={{marginTop:'15px'}}>SENSOR 1</Button>
+          <Button variant="outline-primary" style={{marginTop:'15px'}}>SENSOR 2</Button>
+          <Button variant="outline-primary" style={{marginTop:'15px'}}>SENSOR 3</Button>
+          <Button variant="outline-primary" style={{marginTop:'15px'}}>SENSOR 4</Button>
+          <Button variant="outline-primary" style={{marginTop:'15px'}}>SENSOR 5</Button>
+          
+      </div>
+    )
+  }
+
+}
+
+SideNav.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default SideNav;
